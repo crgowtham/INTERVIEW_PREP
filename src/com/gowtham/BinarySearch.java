8c5a4414040361c302bcba25bf43dc616c2330c3
crgowtham <cr.gowtham@gmail.com>
2018-01-19 00:12:00 -0800

    new files

--- src/com/gowtham/BinarySearch.java
+++ src/com/gowtham/BinarySearch.java
@@ -1,31 +1,31 @@
 package com.gowtham;
 
 public class BinarySearch {
-	
+
 	public static void main(String[] args) {
-		int a[] = {1,2,3,4,5};
-		
+		int a[] = { 1, 2, 3, 4, 5 };
+
 		int result = bs(a, 3);
 		System.out.println("result: " + result);
 	}
-	
+
 	public static int bs(int a[], int x) {
-		int l=0, r=a.length-1;
-		
-		while(l <= r) {
-			int m = l+(r-l)/2;
-			
-			if(x == a[m]) {
+		int l = 0, r = a.length - 1;
+
+		while (l <= r) {
+			int m = l + (r - l) / 2;
+
+			if (x == a[m]) {
 				return m;
-			} 
-			
-			if(a[m] > x) {
-				r = m-1;
 			}
-			
-			l = m+1;
+
+			if (a[m] > x) {
+				r = m - 1;
+			} else {
+				l = m + 1;
+			}
 		}
-			return -1;
+		return -1;
 	}
 
 }
---
+++ src/com/gowtham/SerializeBST.java
@@ -0,0 +1,51 @@
+package com.gowtham;
+
+import java.util.StringTokenizer;
+
+public class SerializeBST {
+	
+	static class TreeNode{
+	    int val;
+	    TreeNode left, right;
+	    TreeNode(int val){
+	        this.val = val;
+	    }
+	}
+
+	public String serialize(TreeNode root){
+	    StringBuilder sb = new StringBuilder();
+	    serialize(root, sb);
+	    return sb.toString();
+	}
+
+	public void serialize(TreeNode x, StringBuilder sb){
+	    if (x == null) {
+	        sb.append("# ");
+	    } else {
+	        sb.append(x.val + " ");
+	        serialzie(x.left, sb);
+	        serialzie(x.right, sb);
+	    }
+	}
+
+	public TreeNode deserialize(String s){
+	    if (s == null || s.length() == 0) return null;
+	    StringTokenizer st = new StringTokenizer(s, " ");
+	    return deserialize(st);
+	}
+
+	private TreeNode deserialize(StringTokenizer st){
+	    if (!st.hasMoreTokens())
+	        return null;
+	    String val = st.nextToken();
+	    if (val.equals("#"))
+	        return null;
+	    TreeNode root = new TreeNode(Integer.parseInt(val));
+	    root.left = deserialize(st);
+	    root.right = deserialize(st);
+	    return root;
+	}
+
+}
+
+
