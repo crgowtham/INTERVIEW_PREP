4c1a9b64867c26f546e5fd94a0c3fca81e419de4
Ranganath, Gowtham (GE Global Research) <Gowtham.Ranganath@ge.com>
2018-07-17 13:42:31 -0700

    newww

--- src/com/gowtham/NQueens.java
+++ src/com/gowtham/NQueens.java
@@ -1,5 +1,7 @@
 package com.gowtham;
 
+import java.util.Arrays;
+
 /* In chess, a queen can move as far as she pleases, horizontally, vertically, or 
  * diagonally. A chess board has 8 rows and 8 columns. The standard 8 by 8 Queen’s problem asks 
  * how to place 8 queens on an ordinary chess board so that none of them can hit any other in one move*/
