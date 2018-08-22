fd261d7053cd2eb9ce5a9d0fc6290e3a7c786bef
Ranganath, Gowtham (GE Global Research) <Gowtham.Ranganath@ge.com>
2018-08-22 13:26:09 -0700

    new files

--- src/com/gowtham/LongestPalindrome.java
+++ src/com/gowtham/LongestPalindrome.java
@@ -5,8 +5,8 @@ public class LongestPalindrome {
 	public String longestPalindrome(String s) {
 		int start = 0, end = 0;
 		for (int i = 0; i < s.length(); i++) {
-			int len1 = expandAroundCenter(s, i, i);
-			int len2 = expandAroundCenter(s, i, i + 1);
+			int len1 = expandAroundCenter(s, i, i); // ODD
+			int len2 = expandAroundCenter(s, i, i + 1); //EVEN
 			int len = Math.max(len1, len2);
 			if (len > end - start) {
 				start = i - (len - 1) / 2;
